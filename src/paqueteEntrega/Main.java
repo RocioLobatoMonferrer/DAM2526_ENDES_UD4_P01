00a2aa7182909bc12a03e796eac83930064665fe
rlobmon782 <rlobmon782@g.educaand.es>
2026-01-09 16:24:01 +0100

    09/01 Main Terminado. Programa registra un Pedido

--- src/paqueteEntrega/Main.java
+++ src/paqueteEntrega/Main.java
@@ -3,7 +3,21 @@ package paqueteEntrega;
 public class Main {
 	public void main() {
 		Pedido pd1 = new Pedido(1);
+		Producto pro1 = new Producto("Teclado", 25.50);
+		Producto pro2 = new Producto("Ratón", 15.00);
+		EstadoPedido estado = null;
 
+		pd1.agregarProducto(pro1);
+		pd1.agregarProducto(pro2);
+
+		System.out.println("PEDIDO:");
+		System.out.println("NÚMERO DE ID: " + pd1.getNumero());
+		System.out.println("ESTADO DEL PEDIDO: " + estado.PENDIENTE);
+		System.out.println("PRECIO TOTAL: " + pd1.calcularTotal());
+
+		pd1.setEstado(estado.ENVIADO);
+
+		System.out.println("\nESTADO DEL PEDIDO: " + pd1.getEstado());
 	}
 
 	public static void main(String[] args) {
